3ba8ba83b6a33bf4c4671d6fec6a937044963664
happyryan <tjdgks120813@gmail.com>
2024-02-23 01:30:50 +0900

    feat: 유저 업데이트 및 삭제 API 에외처리 기능 구현

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -2,13 +2,11 @@ package com.group.libraryapp.controller.user;
 
 import com.group.libraryapp.domain.User;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
+import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -20,7 +18,7 @@ public class UserController {
 
     private final JdbcTemplate jdbcTemplate;
 
-    public  UserController(JdbcTemplate jdbcTemplate) {
+    public UserController(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
@@ -43,5 +41,30 @@ public class UserController {
             }
         });
 
+    }
+
+    @PutMapping("/user")
+    public void updateUser(@RequestBody UserUpdateRequest request) {
+        String readSql = "select * from user where id=?";
+
+        boolean isUserNotExist = jdbcTemplate.query(readSql, (rs, rowNum) -> 0, request.getId()).isEmpty();
+        if (isUserNotExist) {
+            throw new IllegalArgumentException();
+        }
+
+        String sql = "update user set name=? where id=?";
+        jdbcTemplate.update(sql, request.getName(), request.getId());
+    }
+
+    @DeleteMapping("/user")
+    public void deleteUser(@RequestParam String name) {
+        String readSql = "select * from user where name= ?";
+
+        boolean isUserNameNotExist = jdbcTemplate.query(readSql, (rs, rowNum) -> 0, name).isEmpty();
+        if (isUserNameNotExist) {
+            throw new IllegalArgumentException();
+        }
+        String sql = "delete from user where name = ?";
+        jdbcTemplate.update(sql, name);
     }
 }
---
+++ src/main/java/com/group/libraryapp/dto/user/request/UserUpdateRequest.java
@@ -0,0 +1,24 @@
+package com.group.libraryapp.dto.user.request;
+
+public class UserUpdateRequest {
+
+    private long id;
+    private String name;
+
+    public UserUpdateRequest() {
+
+    }
+
+    public UserUpdateRequest(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
